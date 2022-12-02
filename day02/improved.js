786773f59550023033fab37c37df860b33e620fd
Marcus Hansson <machalvan@hotmail.com>
2022-12-02 07:35:33 +0100

    Add improved day 2

---
+++ day02/improved.js
@@ -0,0 +1,32 @@
+const { readFileSync } = require("fs")
+require("../utils")
+
+const part1 = input => {
+  const game = {
+    X: { A: 4, B: 1, C: 7 },
+    Y: { A: 8, B: 5, C: 2 },
+    Z: { A: 3, B: 9, C: 6 },
+  }
+
+  return input.toLines().reduce((acc, line) => {
+    const [opp, me] = line.split(" ")
+    return acc + game[me][opp]
+  }, 0)
+}
+
+const part2 = input => {
+  const game = {
+    X: { A: 3, B: 1, C: 2 },
+    Y: { A: 4, B: 5, C: 6 },
+    Z: { A: 8, B: 9, C: 7 },
+  }
+
+  return input.toLines().reduce((acc, line) => {
+    const [opp, me] = line.split(" ")
+    return acc + game[me][opp]
+  }, 0)
+}
+
+const input = readFileSync("input.txt", "utf8").trim()
+console.log(part1(input))
+console.log(part2(input))
