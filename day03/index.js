bddcf1196fe757631a472c50228e27a5f7b9c41c
Marcus Hansson <machalvan@hotmail.com>
2022-12-03 07:48:33 +0100

    Add day 3

---
+++ day03/index.js
@@ -0,0 +1,33 @@
+const { readFileSync } = require("fs")
+require("../utils")
+
+const part1 = input => {
+  return input.toLines().reduce((acc, line) => {
+    const half = line.length / 2
+    const first = line.slice(0, half).toList()
+    const second = line.slice(half).toList()
+    const intersection = first.intersection(second)[0]
+    const offset = intersection.isUpperCase() ? 38 : 96
+
+    return acc + intersection.charCodeAt(0) - offset
+  }, 0)
+}
+
+const part2 = input => {
+  const lines = input.toLines()
+
+  let res = 0
+  while (lines.length > 0) {
+    const [first, second, third] = lines.splice(0, 3).map(line => line.toList())
+    const intersection = first.intersection(second).intersection(third)[0]
+    const offset = intersection.isUpperCase() ? 38 : 96
+
+    res += intersection.charCodeAt(0) - offset
+  }
+
+  return res
+}
+
+const input = readFileSync("input.txt", "utf8").trim()
+console.log(part1(input))
+console.log(part2(input))
--- utils.js
+++ utils.js
@@ -1,43 +1,56 @@
 // Array
 
 Array.prototype.min = function () {
-  return Math.min(...this);
-};
+  return Math.min(...this)
+}
 
 Array.prototype.max = function () {
-  return Math.max(...this);
-};
+  return Math.max(...this)
+}
 
 Array.prototype.sum = function () {
-  return this.reduce((a, b) => a + b, 0);
-};
+  return this.reduce((a, b) => a + b, 0)
+}
 
 Array.prototype.prod = function () {
-  return this.reduce((a, b) => a * b, 0);
-};
+  return this.reduce((a, b) => a * b, 0)
+}
 
 Array.prototype.sortAsc = function () {
   return this.sort((a, b) =>
     typeof a === "string" ? a.localeCompare(b) : a - b
-  );
-};
+  )
+}
 
 Array.prototype.sortDesc = function () {
   return this.sort((a, b) =>
     typeof a === "string" ? b.localeCompare(a) : b - a
-  );
-};
+  )
+}
 
 Array.prototype.toNumbers = function () {
-  return this.map(str => +str);
-};
+  return this.map(str => +str)
+}
+
+Array.prototype.intersection = function () {
+  return this.filter(str => arguments[0].includes(str))
+}
 
 // String
 
+String.prototype.toList = function () {
+  return this.split("")
+}
+
 String.prototype.toLines = function () {
-  return this.split("\n");
-};
+  return this.split("\n")
+}
 
 String.prototype.toBlocks = function () {
-  return this.split("\n\n").map(block => block.toLines());
-};
+  return this.split("\n\n").map(block => block.toLines())
+}
+
+String.prototype.isUpperCase = function () {
+  "use strict"
+  return this === this.toUpperCase()
+}
