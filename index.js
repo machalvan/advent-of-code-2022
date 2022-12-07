a2e6303c95726aabdb8ad3543b7117c690088484
Marcus Hansson <machalvan@hotmail.com>
2022-12-07 17:56:06 +0100

    Update start file to read files from relative paths

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { readFileSync } = require("fs")
+const { resolve } = require("path")
 
 if (process.argv.length === 2) {
   console.error("Expected an argument.")
@@ -9,13 +10,20 @@ if (process.argv.length === 2) {
 const dayNumber = process.argv[2]
 const day = `day${dayNumber.padStart(2, "0")}`
 
-const input = readFileSync(`${day}/input.txt`, "utf8").trimEnd()
+const input = readFileSync(
+  resolve(__dirname, `${day}/input.txt`),
+  "utf8"
+).trimEnd()
 
 try {
   const { part1, part2 } = require(`./${day}/improved`)
   console.log(part1(input))
   console.log(part2(input))
-} catch (_) {
+} catch (error) {
+  if (error.code !== "MODULE_NOT_FOUND") {
+    throw error
+  }
+
   const { part1, part2 } = require(`./${day}/index`)
   console.log(part1(input))
   console.log(part2(input))
