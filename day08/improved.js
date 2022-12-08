166f91f63f95d6d947af7aefc62642aad00586f5
Marcus Hansson <machalvan@hotmail.com>
2022-12-08 14:11:16 +0100

    Improve day 8 more

--- day08/improved.js
+++ day08/improved.js
@@ -2,47 +2,42 @@ require("../utils")()
 
 const part1 = input => {
   const grid = input.toGrid().map(row => row.toNumbers())
-  let res = 0
 
-  for (let [i, row] of grid.entries()) {
-    for (let [j, tree] of row.entries()) {
-      const left = grid[i].slice(0, j)
-      const right = grid[i].slice(j + 1)
-      const top = grid.map(row => row[j]).slice(0, i)
-      const bottom = grid.map(row => row[j]).slice(i + 1)
+  return grid
+    .map((row, i) =>
+      row
+        .map((tree, j) => {
+          const left = row.slice(0, j)
+          const right = row.slice(j + 1)
+          const top = grid.map(row => row[j]).slice(0, i)
+          const bottom = grid.map(row => row[j]).slice(i + 1)
 
-      if ([left, right, top, bottom].some(dir => tree > Math.max(...dir))) {
-        res += 1
-      }
-    }
-  }
-
-  return res
+          return [left, right, top, bottom].some(dir => tree > Math.max(...dir))
+        })
+        .sum()
+    )
+    .sum()
 }
 
 const part2 = input => {
   const grid = input.toGrid().map(row => row.toNumbers())
 
-  let res = []
-  for (let [i, row] of grid.entries()) {
-    for (let [j, tree] of row.entries()) {
-      const left = grid[i].slice(0, j)
-      const right = grid[i].slice(j + 1)
-      const top = grid.map(row => row[j]).slice(0, i)
-      const bottom = grid.map(row => row[j]).slice(i + 1)
+  return Math.max(
+    ...grid.map((row, i) =>
+      Math.max(
+        ...row.map((tree, j) => {
+          const left = row.slice(0, j)
+          const right = row.slice(j + 1)
+          const top = grid.map(row => row[j]).slice(0, i)
+          const bottom = grid.map(row => row[j]).slice(i + 1)
 
-      res.push(
-        [left.reverse(), right, top.reverse(), bottom]
-          .map(dir => {
-            const i = dir.findIndex(el => el >= tree)
-            return i === -1 ? dir.length : i + 1
-          })
-          .prod()
+          return [left.reverse(), right, top.reverse(), bottom]
+            .map(dir => dir.findIndex(other => other >= tree) + 1 || dir.length)
+            .prod()
+        })
       )
-    }
-  }
-
-  return Math.max(...res)
+    )
+  )
 }
 
 module.exports = { part1, part2 }
