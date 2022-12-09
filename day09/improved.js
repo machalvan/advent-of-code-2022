281e54d931b27fa485dec536a8ec0fd77d166d73
Marcus Hansson <machalvan@hotmail.com>
2022-12-09 12:57:36 +0100

    Add improved day 9

---
+++ day09/improved.js
@@ -0,0 +1,75 @@
+require("../utils")()
+
+const isNeighbour = (x1, y1, x2, y2) =>
+  Math.abs(x2 - x1) <= 1 && Math.abs(y2 - y1) <= 1
+
+const getNewPos = ([x, y], [headX, headY]) => {
+  if (isNeighbour(x, y, headX, headY)) return [x, y]
+
+  const deltaX = headX - x > 0 ? 1 : headX - x < 0 ? -1 : 0
+  const deltaY = headY - y > 0 ? 1 : headY - y < 0 ? -1 : 0
+
+  return [x + deltaX, y + deltaY]
+}
+
+const part1 = input => {
+  const side = 1000
+  const grid = createGrid(side, side)
+  let x = side / 2
+  let y = side / 2
+  const knotsCount = 2
+  const rope = Array(knotsCount).fill([x, y])
+  const xMap = { U: 0, R: 1, D: 0, L: -1 }
+  const yMap = { U: -1, R: 0, D: 1, L: 0 }
+
+  input
+    .toLines()
+    .map(line => line.split(" "))
+    .forEach(([dir, steps]) => {
+      loop(steps, () => {
+        x += xMap[dir]
+        y += yMap[dir]
+
+        loop(knotsCount, i => {
+          rope[i] = i === 0 ? [x, y] : getNewPos(rope[i], rope[i - 1])
+        })
+
+        const [tailX, tailY] = rope.at(-1)
+        grid[tailY][tailX] = 1
+      })
+    })
+
+  return grid.map(row => row.sum()).sum()
+}
+
+const part2 = input => {
+  const side = 1000
+  const grid = createGrid(side, side)
+  let x = side / 2
+  let y = side / 2
+  const knotsCount = 10
+  const rope = Array(knotsCount).fill([x, y])
+  const xMap = { U: 0, R: 1, D: 0, L: -1 }
+  const yMap = { U: -1, R: 0, D: 1, L: 0 }
+
+  input
+    .toLines()
+    .map(line => line.split(" "))
+    .forEach(([dir, steps]) => {
+      loop(steps, () => {
+        x += xMap[dir]
+        y += yMap[dir]
+
+        loop(knotsCount, i => {
+          rope[i] = i === 0 ? [x, y] : getNewPos(rope[i], rope[i - 1])
+        })
+
+        const [tailX, tailY] = rope.at(-1)
+        grid[tailY][tailX] = 1
+      })
+    })
+
+  return grid.map(row => row.sum()).sum()
+}
+
+module.exports = { part1, part2 }
